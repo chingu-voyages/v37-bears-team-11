3a8212800fec163d2b8aa911fd36b9c38ae7af8b
a-w-m <mamrey@gmail.com>
2022-02-18 07:25:17 -0400

    Describe orders table

--- backend/src/database/migrations/20220218095907_orders.js
+++ backend/src/database/migrations/20220218095907_orders.js
@@ -2,14 +2,28 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+
+exports.up = function (knex) {
+	return knex.schema.createTable("orders", (table) => {
+		table.increments("id").primary();
+		table.boolean("paid").notNullable();
+		table.string("status").notNullable();
+		table.integer("food_truck").notNullable();
+		table
+			.foreign("food_truck")
+			.references("id")
+			.inTable("food_trucks")
+			.onDelete("cascade");
+		table.dateTime("datetime_order_placed").notNullable();
+        table.timestamps(true, true);
+
+	});
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+	return knex.schema.dropTable("orders");
 };
