8a77734d903b6acb21927a1aeaafa237cbf9c7d6
a-w-m <mamrey@gmail.com>
2022-02-18 07:26:12 -0400

    Describe cart_menu_items table

--- backend/src/database/migrations/20220218100545_cart_menu_items.js
+++ backend/src/database/migrations/20220218100545_cart_menu_items.js
@@ -2,14 +2,29 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+
+exports.up = function (knex) {
+	return knex.schema.createTable("cart_menu_items", (table) => {
+		table.increments("id").primary();
+		table.integer("user");
+		table
+            .foreign("user")
+            .references("id")
+            .inTable("users")
+            .onDelete("cascade");
+		table.integer("food_truck");
+		table
+			.foreign("food_truck")
+			.references("id")
+			.inTable("food_trucks")
+			.onDelete("cascade");
+	});
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+	return knex.schema.dropTable("cart_menu-items");
 };
