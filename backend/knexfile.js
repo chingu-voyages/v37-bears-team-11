779ec09693688a2c26ae95100a64b4c5bff50d39
a-w-m <mamrey@gmail.com>
2022-02-18 18:44:22 -0400

    Update knexfile configuration

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,42 +1,54 @@
-require('dotenv').config()
-const path = require('path');
+//set environment using .env file
+require("dotenv").config({ path: __dirname + "/.env" });
 
-const {DATABASE_URL_DEVELOPMENT} = process.env
-console.log(DATABASE_URL_DEVELOPMENT)
-// Update with your config settings.
+//point variable to value of connection string in env file
+const { CONNECTION_STRING_DEVELOPMENT } = process.env;
 
 /**
+ * Knex config object
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
 module.exports = {
+	development: {
+		client: "pg",
+		connection: CONNECTION_STRING_DEVELOPMENT,
+		migrations: {
+			directory: "./src/database/migrations",
+		},
+		seeds: {
+			directory: "./src/database/seeds/development",
+		},
+	},
 
-  development: {
-    client: 'pg',
-    connection: DATABASE_URL_DEVELOPMENT,
-
-
-    migrations: {
-      directory: './src/database/migrations'
-    },
-    seeds:{
-      directory: './src/database/seeds/development'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+	staging: {
+		client: "pg",
+		connection: {
+			database: "my_db",
+			user: "username",
+			password: "password",
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: "knex_migrations",
+		},
+	},
+	production: {
+		client: "pg",
+		connection: {
+			database: "my_db",
+			user: "username",
+			password: "password",
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: "knex_migrations",
+		},
+	},
 };
