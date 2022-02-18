5a648b111d8d5824c22c3b8315b36603facb16d9
a-w-m <mamrey@gmail.com>
2022-02-18 07:25:57 -0400

    Describe orders_menu_items table

--- backend/src/database/migrations/20220218100502_orders_menu_items.js
+++ backend/src/database/migrations/20220218100502_orders_menu_items.js
@@ -2,14 +2,32 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+
+exports.up = function (knex) {
+	return knex.schema.createTable("orders_menu_items", (table) => {
+		table.increments("id").primary();
+		table.integer("order");
+		table
+			.foreign("order")
+			.references("id")
+			.inTable("orders")
+			.onDelete("cascade");
+		table.integer("menu_item");
+		table
+			.foreign("menu_item")
+			.references("id")
+			.inTable("menu_items")
+			.onDelete("cascade");
+        table.integer('quantity').notNullable()
+        table.timestamps(true, true);
+
+	});
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+	return knex.schema.dropTable("orders_menu_items");
 };
