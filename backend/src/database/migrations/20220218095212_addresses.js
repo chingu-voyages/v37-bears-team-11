5003f524b0a15d45cbd71f11dc46e7584b0eba48
a-w-m <mamrey@gmail.com>
2022-02-18 06:19:32 -0400

    Describe addresses table

--- backend/src/database/migrations/20220218095212_addresses.js
+++ backend/src/database/migrations/20220218095212_addresses.js
@@ -3,7 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('addresses', (table)=>{
+        table.increments('id').primary()
+        table.string('street').notNullable()
+        table.string('city').notNullable()
+        table.string('state').notNullable()
+        table.string('country').notNullable()
+    })
 };
 
 /**
@@ -11,5 +17,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('addresses')
 };
