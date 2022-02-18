9054cb06e83497309db3d8f6f54730776b1cf9dc
a-w-m <mamrey@gmail.com>
2022-02-18 07:25:31 -0400

    Describe menu_items table

--- backend/src/database/migrations/20220218095928_menu_items.js
+++ backend/src/database/migrations/20220218095928_menu_items.js
@@ -2,14 +2,26 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+
+exports.up = function (knex) {
+	return knex.schema.createTable("menu_items", (table) => {
+		table.increments("id").primary();
+		table.integer("food_truck");
+		table
+			.foreign("food_truck")
+			.references("id")
+			.inTable("food_trucks")
+			.onDelete("cascade");
+		table.string("name").notNullable();
+		table.integer("price").notNullable();
+		table.timestamps(true, true);
+	});
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+	return knex.schema.dropTable("menu_items");
 };
