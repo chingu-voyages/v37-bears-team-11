ffb8cbee5b3ca14263d8231a0dfaa0ebc5992de2
a-w-m <mamrey@gmail.com>
2022-03-01 17:08:53 -0400

    Update styles for Home component

--- frontend/src/ui/pages/Home/Home.tsx
+++ frontend/src/ui/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div className='flex flex-col bg-red-500 w-screen h-screen'>
             <div className='flex bg-home h-1/3 bg-cover w-full'>
-                <div>Troka</div>
+                <div className='text-3xl text-white font-medium px-5 py-3'>Troka</div>
             </div>
         </div>
     )
