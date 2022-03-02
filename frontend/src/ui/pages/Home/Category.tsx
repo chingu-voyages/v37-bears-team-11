eb5a820ab4999c577e416556218739548d0823ca
a-w-m <mamrey@gmail.com>
2022-03-02 12:14:08 -0400

    Add onClick event to Category

--- frontend/src/ui/pages/Home/Category.tsx
+++ frontend/src/ui/pages/Home/Category.tsx
@@ -8,12 +8,12 @@ type CategoryProps = {
 function Category(props: CategoryProps) {
     const { svg, title } = props
 
-    function handleClick(e: Event): void {
+    function handleClick(e: React.MouseEvent<HTMLDivElement>): void {
         //handle request to backend on click on click
     }
 
     return (
-        <div className='flex flex-col gap-3 items-center'>
+        <div className='flex flex-col gap-3 items-center' onClick={(e) => handleClick(e)}>
             {svg}
             <p className='text-sm'>{title}</p>
         </div>
