c69da545a94683242a07ce31fb0d42ee8d16f554
a-w-m <mamrey@gmail.com>
2022-03-02 12:28:26 -0400

    Update font in Home component

--- frontend/src/ui/pages/Home/Home.tsx
+++ frontend/src/ui/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div className='flex flex-col w-screen h-screen'>
             <div className='flex bg-home bg-cover bg-no-repeat h-1/3'>
-                <div className='text-3xl text-white font-medium px-5 py-3'>Troka</div>
+                <div className='text-3xl text-white font-mplus font-medium px-5 py-3'>Troka</div>
             </div>
             <Categories />
         </div>
