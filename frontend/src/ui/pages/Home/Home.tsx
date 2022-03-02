d50c10748f61bd2210f4cbdb896c44e6a418f92b
a-w-m <mamrey@gmail.com>
2022-03-02 12:15:05 -0400

    Render Categories in Home component

--- frontend/src/ui/pages/Home/Home.tsx
+++ frontend/src/ui/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Categories from './Categories'
 
 function Home() {
     return (
-        <div className='flex flex-col bg-red-500 w-screen h-screen'>
-            <div className='flex bg-home h-1/3 bg-cover w-full'>
+        <div className='flex flex-col w-screen h-screen'>
+            <div className='flex bg-home bg-cover bg-no-repeat h-1/3'>
                 <div className='text-3xl text-white font-medium px-5 py-3'>Troka</div>
             </div>
+            <Categories />
         </div>
     )
 }
