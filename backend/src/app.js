a784be498bae4f80a8360e69979f5600cc0a086e
a-w-m <mamrey@gmail.com>
2022-03-06 10:17:17 -0400

    Fetch data with Search component - temporarily fix cross origin request in server - create search api service - create sample route /search/* - render search results in Home

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,40 @@
 const express = require("express");
+
 //create express app
 const app = express();
 //require knex function to communicate with database
 const knex = require("./database/connection");
 
 //configure app
+//handle cors error (temporary fix) 
+ app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
 
-/**
- * sample route
+  /**
+ * sample routes
  * create route to GET all users from database
+ * create route to Get all food trucks
  **/
+
 
 app.get("/users", async (req, res) => {
 	try {
 		//query database using knex function
 		const data = await knex("users").select("*");
 		return res.json(data);
+	} catch (err) {
+		return res.send("error");
+	}
+});
+
+app.get("/search/*", async (req, res) => {
+	try {
+		//query database using knex function
+		const data = await knex("food_trucks").select("*");
+		return res.json({data});
 	} catch (err) {
 		return res.send("error");
 	}
