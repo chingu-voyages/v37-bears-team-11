ab92de45721652451c6b3c12d3a8e5b6f5bf6bb2
a-w-m <mamrey@gmail.com>
2022-03-11 15:41:55 -0400

    Add index to food_trucks and items name column

---
+++ backend/src/database/migrations/20220311185950_add_index_food_trucks.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('food_trucks', (table)=>{
+      table.index('name')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.table('food_trucks', (table)=>{
+        table.dropIndex('name')
+    })
+  
+};
