15e282d668e18d6a1fd1b4ac448d310b5f86e12c
a-w-m <mamrey@gmail.com>
2022-03-13 11:49:53 -0400

    Update App for Search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,33 @@
 import React, { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 //components
+import { SearchProvider } from './ui/components/search/SearchProvider'
 import Layout from './ui/components/Layout'
 import LoadingPage from './ui/pages/loading/LoadingPage'
 import Login from './ui/pages/registration/Login'
 import Registration from './ui/pages/registration/Registration'
 import Home from './ui/pages/Home/Home'
+import SearchResultsList from './ui/components/searchResults/SearchResultsList'
 
 function App() {
     const [loading, setLoading] = useState(true)
 
     return (
         <div className='App'>
-            <Routes>
-                {loading ? (
-                    <Route path='/' element={<LoadingPage setLoading={setLoading} />} />
-                ) : (
-                    <Route path='/' element={<Layout />}>
-                        <Route index element={<Login />} />
-                        <Route path='register' element={<Registration />} />
-                        <Route path='home' element={<Home />} />
-                    </Route>
-                )}
-            </Routes>
+            <SearchProvider>
+                <Routes>
+                    {loading ? (
+                        <Route path='/' element={<LoadingPage setLoading={setLoading} />} />
+                    ) : (
+                        <Route path='/' element={<Layout />}>
+                            <Route index element={<Login />} />
+                            <Route path='register' element={<Registration />} />
+                            <Route path='home' element={<Home />} />
+                            <Route path='/search' element={<SearchResultsList />} />
+                        </Route>
+                    )}
+                </Routes>
+            </SearchProvider>
         </div>
     )
 }
